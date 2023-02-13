13ad14b0537f7d3b4c95a5f0780fb2e3d67baafe
Vasyl Dzen <vasyl.dzen@gmail.com>
2023-02-13 19:17:05 +0200

    updated wheel

---
+++ build/lib/alerts_in_ua/user_agent.py
@@ -0,0 +1,5 @@
+from alerts_in_ua import __version__
+class UserAgent:
+    @staticmethod
+    def get_user_agent():
+        return f"Alerts.in.ua API Client v{__version__} (compatible; alerts.in.ua/{__version__}; +https://alerts.in.ua)"
