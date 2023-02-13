9e77548e5622ffa726b51a81302d01f158ad09e1
Vasyl Dzen <vasyl.dzen@gmail.com>
2023-02-13 19:16:09 +0200

    fixed user_agent generation

--- alerts_in_ua/user_agent.py
+++ alerts_in_ua/user_agent.py
@@ -2,4 +2,4 @@ from alerts_in_ua import __version__
 class UserAgent:
     @staticmethod
     def get_user_agent():
-        return f"Alerts.in.ua API Library v{__version__} (compatible; alerts.in.ua/{__version__}; +https://alerts.in.ua)"
+        return f"Alerts.in.ua API Client v{__version__} (compatible; alerts.in.ua/{__version__}; +https://alerts.in.ua)"
